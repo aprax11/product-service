ce82b9b1d86a005bf548e17178e0cd4c2cca6686
aprax11 <jashaspringmann@web.de>
2023-09-29 15:09:12 +0200

    added Redis

---
+++ src/main/java/com/example/demo/port/config/RedisConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.demo.port.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+public class RedisConfiguration {
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(2))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+}
